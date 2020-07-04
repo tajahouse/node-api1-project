b7bcf2284140a6b93bd286f5c4b21ea209eea32c
Taja House <ahlliahtaja@yahoo.com>
2020-07-04 11:25:25 -0700

    Downloaded dependencies and started database

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // implement your API here
+const express = require("express");
+
+const server = express();
+
+const port = 4040
+
+server.get("/", (req, res)=>{
+    res.json(
+        {
+            message: "You've got this Taja!"
+        }
+    )
+})
+
+
+
+server.listen(port, ()=>{
+    console.log(`Server listening on port ${port}`)
+})
