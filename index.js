05118b00f769cf0a9ce2f58b52556baa300b571e
Taja House <ahlliahtaja@yahoo.com>
2020-07-05 08:39:15 -0700

    Having issues with structure of data

--- index.js
+++ index.js
@@ -1,20 +1,73 @@
 // implement your API here
 const express = require("express");
+const datab = require("./data/db");
+const db = require("./data/db");
 
-const server = express();
+const server = express((request, response)=>{
+    request.setHeader("Content-Type", "application/json")
+});
 
-const port = 4040
+const port = 3001
 
-server.get("/", (req, res)=>{
-    res.json(
-        {
-            message: "You've got this Taja!"
+    server.get("/api/users", (req, res)=>{
+        const users = datab.find() 
+        users ? 
+        res.json(users) 
+            : res.status(500)
+    })
+
+    server.get('/api/users/:id', (req, res)=>{
+        const user = db.findById();
+    user ?
+        res.json(user) 
+            : res.status(404)
+    })
+
+    server.post('/api/users', (req, res)=>{
+        if(!req.body.name && !req.body.bio){
+            return res.statusCode(400).json({
+                message: "Please add a name and bio for the user"
+            })
         }
-    )
-})
+        const newUser = db.insert({
+        name: req.body.name,
+        bio: req.body.bio
+        })
+        res.status(201).json(newUser)
+    })
 
-
+    server.put('/api/users/:id', (req, res)=>{
+        const user = db.findById();
+        if(user){
+            const updateUser = db.update(user.id, {
+                name: req.body.name || user.name,
+                bio: req.body.bio || user.bio
+            })
+            res.json(updateUser)
+        } else {
+            res.status(404).json({
+                message: "User not found"
+            })
+        }
+    
+    })
+    server.delete('/api/users/:id', (req, res)=>{
+        const user = db.findById();
+        if(user){
+            const deleteUser = db.delelte(user.id, {
+                name: req.body.name || user.name,
+                bio: req.body.bio || user.bio
+            })
+            res.json(deleteUser)
+        } else {
+            res.status(404).json({
+                message: "User not found"
+            })
+        }
+    
+    })
 
 server.listen(port, ()=>{
     console.log(`Server listening on port ${port}`)
 })
+
