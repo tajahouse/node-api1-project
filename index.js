091bf0a275cf0389e0ad0fb91375235edc1651ef
Taja House <ahlliahtaja@yahoo.com>
2020-07-08 05:46:36 -0700

    Using async/await functions

--- index.js
+++ index.js
@@ -12,44 +12,40 @@ const port = 3001
 //GET
 //---------------------------------------------------------------------------------------------------------------------------
 server.get('/', (req, res) => {
-    res.send('You`\ve got this Taja');
+    res.send('You\'ve got this Taja');
 });
 
+server.get("/api/users", async (req, res)=>{
+    const users =  await db.find()
+        users ?
+        res.json(users) 
+        : res.status(500)
+            .json({
+                     message: 'The user information could not be retrieved.',
+                 });
+    
+     })
 
-    server.get("/api/users", (req, res)=>{
-    db.find()
-        .then(users => 
-            res.status(200).json(users))
-                .catch(err => {
-                    console.log(err);
-                    res.status(500).json({
-                        message: 'The user information could not be retrieved.',
-                    });
-                });
-    })
-
-    server.get('/api/users/:id', (req, res)=>{
-     db.findById(req.params.id)
-     .then(users =>
-        res.status(404).json(users))
-        .catch(err =>{
-            console.log(err);
-            res.status(404).json({
-                message: 'The user with the specified ID does not exist.'
-            })
-        })
-     
+    server.get('/api/users/:id', async (req, res)=>{
+        const users = await db.findById()
+            users ?
+            res.json(users)
+            : res.status(404)
+            .json({
+                      message: 'The user with the specific ID does not exist.'
+                  })
+  
     })
 
     //POST
     //---------------------------------------------------------------------------------------------------------------------------  
-    server.post('/api/users', (req, res)=>{
+    server.post('/api/users', async (req, res)=>{
         if(!req.body.name && !req.body.bio){
             return res.statusCode(400).json({
                 message: "Please add a name and bio for the user"
             })
         }
-        const newUser = db.insert({
+        const newUser = await db.insert({
         name: req.body.name,
         bio: req.body.bio
         })
@@ -58,8 +54,8 @@ server.get('/', (req, res) => {
 
     //PUT
     //---------------------------------------------------------------------------------------------------------------------------
-    server.put('/api/users/:id', (req, res)=>{
-        const user = db.findById();
+    server.put('/api/users/:id', async (req, res)=>{
+        const user = await db.findById();
         if(user){
             const updateUser = db.update(user.id, {
                 name: req.body.name || user.name,
@@ -90,6 +86,7 @@ server.get('/', (req, res) => {
         }
     
     })
+   
 
 server.listen(port, ()=>{
     console.log(`Server listening on port ${port}`)
